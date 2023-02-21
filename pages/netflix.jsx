00e93aaae78974dea8df2118387f3a82d6df8176
Quangtincntt <boluclac19@gmail.com>
2023-02-21 23:04:45 +0700

    update

--- pages/netflix.jsx
+++ pages/netflix.jsx
@@ -1,70 +1,75 @@
-import Image from 'next/image';
-import React from 'react';
-import tiktokImg from '../public/assets/projects/tiktok.jpg';
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
+import Image from "next/image";
+import React from "react";
+import tiktokImg from "../public/assets/projects/tiktok.jpg";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
 
 const netflix = () => {
   return (
-    <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
         <Image
-          className='absolute z-1'
-          layout='fill'
-          objectFit='cover'
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
           src={tiktokImg}
-          alt='/'
+          alt="/"
         />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Netflix App</h2>
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Tik Tok UI</h2>
           <h3>React JS</h3>
         </div>
       </div>
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4'>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
           <p>Project</p>
-          <h2>Overview</h2>
-         
+          <p>( Under construction )</p>
           <a
-            href='https://github.com/Quangtincntt/tiktok-ui'
-            target='_blank'
-            rel='noreferrer'
+            href="https://github.com/Quangtincntt/tiktok-ui"
+            target="_blank"
+            rel="noreferrer"
           >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+            <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href='https://tiktok-ui-eosin.vercel.app/'
-            target='_blank'
-            rel='noreferrer'
+            href="https://tiktok-ui-eosin.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
           >
-            <button className='px-8 py-2 mt-4'>Demo</button>
+            <button className="px-8 py-2 mt-4">Demo</button>
           </a>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> React
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Search User
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Tailwind
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Axios
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Javascript
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Classnames
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Firebase
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Customize Webpack
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> IMDB API
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Babel Plugin
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> PropTypes library
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Custom React Hook 
               </p>
             </div>
           </div>
         </div>
-        <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
         </Link>
       </div>
     </div>
