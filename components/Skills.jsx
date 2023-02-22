507c9d79374a975731154e5f4a060da1db82347c
Quangtincntt <boluclac19@gmail.com>
2023-02-22 22:29:03 +0700

    update

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,6 @@
 import Image from "next/image";
-import React from "react";
-import Html from "../public/assets/skills/html.png";
-import Css from "../public/assets/skills/css.png";
-import Javascript from "../public/assets/skills/javascript.png";
-import ReactImg from "../public/assets/skills/react.png";
-import Tailwind from "../public/assets/skills/tailwind.png";
-import Github from "../public/assets/skills/github1.png";
-import NextJS from "../public/assets/skills/nextjs.png";
-import Typescript from "../public/assets/skills/typescript.png";
-import bootstraps from "../public/assets/skills/boostraps.jpg";
-import redux from "../public/assets/skills/redux.png";
+import { arraySkills } from './index';
+
 
 const Skills = () => {
   return (
@@ -20,107 +11,22 @@ const Skills = () => {
         </p>
         <h2 className="py-4">What I Can Do</h2>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Html} width="64px" height="64px" alt="/" />
+          {arraySkills.map((skills, index) => {
+            const {title, img} = skills
+            return (
+              <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300" key={index}>
+                <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                  <div className="m-auto">
+                    <Image src={img} width="64px" height="64px" alt="/" />
+                  </div>
+                  <div className="flex flex-col items-center justify-center">
+                    <h3 className="ml-4">{title}</h3>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
-              </div>
-            </div>
+            )
+          })}
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Css} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Javascript} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>JavaScript</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={ReactImg} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>React</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Tailwind} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Typescript} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Typescript</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Github} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Github</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={NextJS} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Next</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={bootstraps} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>bootstraps</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={redux} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Redux</h3>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
---
+++ components/index.jsx
@@ -0,0 +1,63 @@
+import Html from "../public/assets/skills/html.png";
+import Css from "../public/assets/skills/css.png";
+import Javascript from "../public/assets/skills/javascript.png";
+import ReactImg from "../public/assets/skills/react.png";
+import Tailwind from "../public/assets/skills/tailwind.png";
+import Github from "../public/assets/skills/github1.png";
+import NextJS from "../public/assets/skills/nextjs.png";
+import Typescript from "../public/assets/skills/typescript.png";
+import bootstraps from "../public/assets/skills/boostraps.jpg";
+import redux from "../public/assets/skills/redux.png";
+import shopify from "../public/assets/skills/shopify.png";
+import firebase from "../public/assets/skills/firebase.png";
+
+export const arraySkills = [
+    {
+      title: "HTML",
+      img: Html
+    },
+    {
+      title: "CSS",
+      img: Css,
+    },
+    {
+      title: "JavaScript",
+      img: Javascript ,
+    },
+    {
+      title: "React",
+      img: ReactImg,
+    },
+    {
+      title: "Tailwind",
+      img: Tailwind,
+    },
+    {
+      title: "Typescript",
+      img: Typescript,
+    },
+    {
+      title: "Github",
+      img: Github,
+    },
+    {
+      title: "Next",
+      img: NextJS,
+    },
+    {
+      title: "Bootstraps",
+      img: bootstraps ,
+    },
+    {
+      title: "Redux",
+      img: redux,
+    },
+    {
+      title: "Shopify",
+      img: shopify,
+    },
+    {
+      title: "Firebase",
+      img: firebase,
+    },
+  ];
